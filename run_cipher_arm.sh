426ebdf3e75555b7a044a88d7caa766fb4826bc3
jcsf <jcsf_1995@hotmail.com>
2018-07-12 19:40:38 +0100

    Changed script to test metrics

--- run_cipher_arm.sh
+++ run_cipher_arm.sh
@@ -1,6 +1,6 @@
 # $1 - Cipher
 # $2 - Senario
-# $3 - Debug Mode
+# $3 - Compiler Option
 # $4 - Execute Mode (On/Off)
 
 cd /home/felics/FELICS/FELICS_v1.1.23/block_ciphers/source/ciphers/$1/source
@@ -8,7 +8,7 @@ make -f ./../../../common/cipher.mk cleanall
 
 if [ $4 = 1 ]
 then
-    make -f ./../../../common/cipher.mk DEBUG=$3 ARCHITECTURE=ARM SCENARIO=$2
-    make -f ./../../../common/cipher.mk ARCHITECTURE=ARM SCENARIO=$2 upload-cipher
+    make -f ./../../../common/cipher.mk DEBUG=7 ARCHITECTURE=ARM SCENARIO=$2 COMPILER_OPTIONS=$3 
+    make -f ./../../../common/cipher.mk ARCHITECTURE=ARM SCENARIO=$2 COMPILER_OPTIONS=$3 upload-cipher
     ./../../../../../common/scripts/arm/arm_serial_terminal.py
 fi
--- run_collect_metrics.sh
+++ run_collect_metrics.sh
@@ -29,7 +29,7 @@ ciphers="NOEKEON_128_128_v03 NOEKEON_128_128_v04 NOEKEON_128_128_v05 NOEKEON_128
 ciphers="RECTANGLE_64_128_v01 RECTANGLE_64_128_v10 RECTANGLE_64_128_v11 RECTANGLE_64_128_v12 RECTANGLE_64_128_v13 RECTANGLE_64_128_v14 RECTANGLE_64_128_v15"
 
 # Comparision Between SPECK Reference Implementation, SPECK Full Unroll, SPECK Register Variables, SPECK Register Variables Unroll
-ciphers="Speck_64_128_v07 Speck_64_128_v08 Speck_64_128_v09 Speck_64_128_v10"
+ciphers="Speck_128_128_v01 Speck_64_128_v07 Speck_64_128_v08 Speck_64_128_v09 Speck_64_128_v10 Speck_64_128_v11"
 
 # Comparision Between SPARX Normal, SPARX 32-bits Speckey w/ Pointers, SPARX 32-bits Speckey w/ Return Value, SPARX 32-bits Speckey Inlined, SPARX 32-bits Full Unroll, SPARX 32-bits State In Registers and Steps in Cycles, SPARX 32-bits State in Registers, SPARX 32-bits State in Registers Full Unroll
 ciphers="SPARX_64_128_v36 SPARX_64_128_v37 SPARX_64_128_v38 SPARX_64_128_v39 SPARX_64_128_v40 SPARX_64_128_v41 SPARX_64_128_v42 SPARX_64_128_v43"
@@ -41,9 +41,9 @@ ciphers="PRESENT_64_80_v01 PRESENT_64_80_v07 PRESENT_64_80_v08 PRESENT_64_80_v09
 #ciphers="CLEFIA_128_128_v02 CLEFIA_128_128_v04 CLEFIA_128_128_v05 CLEFIA_128_128_v06 CLEFIA_128_128_v07 CLEFIA_128_128_v08 CLEFIA_128_128_v09 CLEFIA_128_128_v10 CLEFIA_128_128_v11 AES_128_128_v02 AES_128_128_v08 AES_128_128_v09 AES_128_128_v10 AES_128_128_v11 RoadRunneR_64_128_v02 RoadRunneR_64_128_v07 RoadRunneR_64_128_v08 RoadRunneR_64_128_v09 RoadRunneR_64_128_v10 RoadRunneR_64_128_v11 RoadRunneR_64_128_v12 RoadRunneR_64_128_v13 RoadRunneR_64_128_v14 RoadRunneR_64_128_v15 RoadRunneR_64_128_v16 RoadRunneR_64_128_v17 RoadRunneR_64_128_v18 NOEKEON_128_128_v03 NOEKEON_128_128_v04 NOEKEON_128_128_v05 NOEKEON_128_128_v06 NOEKEON_128_128_v07 NOEKEON_128_128_v08 NOEKEON_128_128_v09 NOEKEON_128_128_v10 NOEKEON_128_128_v11 NOEKEON_128_128_v12 RECTANGLE_64_128_v10 RECTANGLE_64_128_v11 RECTANGLE_64_128_v12 RECTANGLE_64_128_v13 RECTANGLE_64_128_v14 Speck_64_128_v07 Speck_64_128_v08 Speck_64_128_v09 Speck_64_128_v10 SPARX_64_128_v36 SPARX_64_128_v37 SPARX_64_128_v38 SPARX_64_128_v39 SPARX_64_128_v40 SPARX_64_128_v41 SPARX_64_128_v42 SPARX_64_128_v43"
 
 #ciphers="Speck_64_128_v07 Speck_64_128_v08 Speck_64_128_v09 Speck_64_128_v10 Speck_64_128_v11 Speck_64_128_v12"
-ciphers="AES_128_128_v01 AES_128_128_v08 AES_128_128_v09 AES_128_128_v10 AES_128_128_v11 AES_128_128_v12 AES_128_128_v13 AES_128_128_v14 AES_128_128_v15"
+#ciphers="AES_128_128_v01 AES_128_128_v08 AES_128_128_v09 AES_128_128_v10 AES_128_128_v11 AES_128_128_v12 AES_128_128_v13 AES_128_128_v14 AES_128_128_v15"
 
-ciphers="AES_128_128_v08 AES_128_128_v09"
+ciphers="Speck_64_128_v12"
 
 cd /home/felics/FELICS/FELICS_v1.1.23/block_ciphers/scripts/
 
@@ -51,7 +51,8 @@ for i in $ciphers; do
     find ../source/ciphers/$i/build -type f -not -name '.gitignore' -delete
 done
 
-./collect_ciphers_metrics.sh -f='5' -a='ARM' --scenarios='1' --ciphers="$ciphers"
+#./collect_ciphers_metrics.sh -f='5' -a='ARM' --scenarios='1' --ciphers="$ciphers"
+./collect_ciphers_metrics.sh -f='5' -a='ARM' --scenarios='0' --ciphers="$ciphers"
 
 for i in $ciphers; do
     find ../source/ciphers/$i/build -type f -not -name '.gitignore' -delete
